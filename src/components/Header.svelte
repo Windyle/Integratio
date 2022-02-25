2bc86f815c7ad5128addb4d6b815d41627b0717b
a.denti@errevi.com <a.denti@errevi.com>
2022-02-25 15:25:12 +0100

    Transferred to Electron

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,69 +1,77 @@
 <script lang="ts">
-	import { currentInstance } from "../stores/InstanceStore.js";
+  import { currentInstance } from "../stores/InstanceStore.js";
 
-	let instance: string;
-	currentInstance.subscribe(value => {
-		instance = value;
-	});
+  let instance: string;
+  currentInstance.subscribe((value) => {
+    instance = value;
+  });
 </script>
 
 <header>
-	<img alt="Logo" class="logo" src="../../static/icon-borderless.svg" />
-	<h1>INTEGRATIO</h1>
-	<h2>{instance}</h2>
-	<img alt="Help Button" class="help" src="../../static/help-circle.svg" on:click={() => window.open('https://github.com/Windyle/Integratio/issues', '_blank').focus()} />
+  <img alt="Logo" class="logo" src="../static/icon-borderless.svg" />
+  <h1>INTEGRATIO</h1>
+  <h2>{instance}</h2>
+  <img
+    alt="Help Button"
+    class="help"
+    src="../static/help-circle.svg"
+    on:click={() =>
+      window
+        .open("https://github.com/Windyle/Integratio/issues", "_blank")
+        .focus()}
+  />
 </header>
 
 <style>
-	header {
-		background-color: var(--primary);
-		height: 35px;
-		width: 100%;
-		position: fixed;
-		top: 0;
-		left: 0;
-		z-index: 1;
-		display: flex;
-		align-items: center;
-	}
+  header {
+    background-color: var(--primary);
+    height: 35px;
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+  }
 
-	.logo {
-		width: 28px;
-		margin-left: 15px;
-		user-select: none;
-		-webkit-user-drag: none;
-	}
+  .logo {
+    width: 28px;
+    margin-left: 15px;
+    user-select: none;
+    -webkit-user-drag: none;
+  }
 
-	h1 {
-		margin-left: 10px;
-		color: var(--text);
-		font-family: var(--font-family);
-		font-weight: 400;
-		font-size: 1em;
-		user-select: none;
-		-webkit-user-drag: none;
-		opacity: 0.8;
-	}
+  h1 {
+    margin-left: 10px;
+    color: var(--text);
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 1em;
+    user-select: none;
+    -webkit-user-drag: none;
+    opacity: 0.8;
+  }
 
-	h2 {
-		margin-left: 10px;
-		color: var(--input-text);
-		font-family: var(--font-family);
-		font-weight: 400;
-		font-size: 0.9em;
-		user-select: none;
-		-webkit-user-drag: none;
-		transform: translateY(-1px);
-	}
+  h2 {
+    margin-left: 10px;
+    color: var(--input-text);
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 0.9em;
+    user-select: none;
+    -webkit-user-drag: none;
+    transform: translateY(-1px);
+  }
 
-	.help {
-		margin-left: auto;
-		margin-right: 11px;
-		cursor: pointer;
-		-webkit-user-drag: none;
-	}
+  .help {
+    margin-left: auto;
+    margin-right: 11px;
+    cursor: pointer;
+    -webkit-user-drag: none;
+  }
 
-	.help:hover {
-		opacity: 0.5;
-	}
+  .help:hover {
+    opacity: 0.5;
+  }
 </style>
